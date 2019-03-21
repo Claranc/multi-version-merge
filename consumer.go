379c22eddb02c7561516a99a4ccd63a9f3cfdc36
Claranc <fxj_bupt@163.com>
2019-03-21 10:37:41 +0800

    v1.2

--- consumer.go
+++ consumer.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"math/rand"
 	"sync"
 	"time"
 )
 
+//并发安全字典存储数据包，一边存储一边取走。key为VersionId,value为当前收到的VersionId下的所有数据包
 var mp sync.Map
+//记录消费者受到数据的当前最小VersionId
 var minId int64 = 0
 
 // Consumer consumes versioned data which produced by multi Producer
@@ -33,41 +34,35 @@ func NewConsumer(channels []<-chan *VersionedData, streamCount int64) *Consumer 
 // Start starts consuming versioned data
 func (c *Consumer) Start(ctx context.Context) {
 	// TODO: implement this to consume data from c.dataChannels, and merged result should send to c.ch
+	//将收到的数据存储到map中
 	go c.Receive(ctx)
+	//等待一小会，等map中已经开始有一些数据了再开始处理
+	time.Sleep(time.Millisecond*200)
 	log.Println("receive start:")
-	count := 0;
-	time.Sleep(time.Millisecond*20)
 	for {
+		//取出map中当前VersionId下的所有数据包切片
 		v1,ok := mp.Load(minId)
 		if ok {
-			//fmt.Println("min = ", minId)
 			curData,_ := v1.([]*VersionedData)
+			//如果当前VersionId下的数据包已经被取完了，就把key值加1，取下一个版本号的value
 			if curData == nil {
-				if count < 2 {
-					time.Sleep(time.Millisecond*10)
-					count++
-				} else {
-					minId++
-				}
+				minId++
 				continue
 			}
+			//取走当前VersionId下的数据包切片中的第一个数据，发送到c.ch
 			sendData := curData[0]
-			//fmt.Println("len = ", len(curData))
+			//如果这恰好是最后一个数据，取走后就把value设为空值，否则就删掉第一个
 			if len(curData) == 1 {
 				curData = nil
 			} else {
 				curData = curData[1:]
 			}
+			//重新存进map里
 			mp.Store(minId, curData)
 			c.ch<- sendData
 		} else {
-			fmt.Println("cannot find versionID of ", minId)
-			if count < 2 {
-				time.Sleep(time.Millisecond*10)
-				count++
-			} else {
-				minId++
-			}
+			//如果没找到key对应的value，表明还没有该版本的数据包发送过来，跳过吧
+			minId++
 		}
 	}
 
@@ -75,27 +70,25 @@ func (c *Consumer) Start(ctx context.Context) {
 
 func (c *Consumer) Receive(ctx context.Context) {
 	for {
-		var em *VersionedData
-		t := rand.New(rand.NewSource(time.Now().UnixNano()))
-		temp := t.Intn(5)
-		//fmt.Println("rand = ", temp)
+		var em *VersionedData //定义当前从生产者数据通道接收到的一个数据包
+		t := rand.New(rand.NewSource(time.Now().UnixNano())) //随机种子
 		select {
 		case <-ctx.Done():
 			return
-		case em = <-c.dataChannels[temp]:
+		case em = <-c.dataChannels[t.Intn(len(c.dataChannels))]:  //随机从一个生产者通道接收一个数据包
 		}
+		//将该数据包存进map里
 		num := em.VersionID
-		//fmt.Println("num = ", num)
 		v1, ok := mp.Load(num)
 		if ok {
+			//如果map里有该versionId对应的数据包，就添加到最后
 			curData,_ := v1.([]*VersionedData)
 			curData = append(curData, em)
 			mp.Store(num, curData)
-			//fmt.Printf("key = %d, value = %v /n", num, *curData[1])
 		} else {
+			//如果是该versionId对应的第一个数据包，就直接存进来
 			curData := []*VersionedData{em}
 			mp.Store(num, curData)
-			//fmt.Println("add data to map")
 		}
 
 	}
--- verifier.go
+++ verifier.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log"
+	"os"
 )
+
+//记录通道中的当前versionId
+var curId int64 = 0
 
 // Verifier verify whether Consumer have merged versioned data
 // NOTE: the implementation for Verifier is optional
@@ -22,12 +28,29 @@ func NewVerifier(ch <-chan *VersionedData) *Verifier {
 // Start starts verifying
 func (v *Verifier) Start(ctx context.Context) {
 	// TODO: implement this to verify data from v.ch
+	output, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC,0644)
+	defer output.Close()
+
+	if err != nil {
+		log.Fatal("open file failed")
+	}
+	outputwriter := bufio.NewWriter(output)
 	for{
 		select {
 		case <-ctx.Done():
 			return
-		case t := <-v.ch:
-			fmt.Printf("versionId = %d, producerId = %d, streamId = %d, data = %s\n", t.VersionID, t.ProducerID,t.StreamID, t.Data )
+		case t:=<-v.ch:
+			//将输出通道的数据包数据打印到日志中
+			s := fmt.Sprintf("versionId = %d, producerId = %d, streamId = %d, data = %s\n", t.VersionID, t.ProducerID,t.StreamID, t.Data )
+			outputwriter.WriteString(s)
+			outputwriter.Flush()
+			if(t.VersionID > curId) {
+				//如果收到更高版本的数据，更新当前的curId
+				curId = t.VersionID
+			} else if (t.VersionID < curId) {
+				//如果出现了新的数据包小于当前versionId，则代表错误，程序有问题
+				log.Fatal("output wrong")
+			}
 		}
 	}
 
